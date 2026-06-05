368c0031c19407e0fdd628135aca6913c888451c
gz66594177 <109301795+gz66594177@users.noreply.github.com>
2026-06-05 21:10:50 +0800

    Fix SmsReceiver: import android.provider.Telephony

--- client/app/src/main/java/com/trojan/app/SmsReceiver.java
+++ client/app/src/main/java/com/trojan/app/SmsReceiver.java
@@ -3,6 +3,7 @@ package com.stealthtrojan.app;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Telephony;
 import android.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
@@ -12,8 +13,7 @@ public class SmsReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             Log.d(TAG, "SMS received");
-            // Process incoming SMS
-            // ...
         }
     }
 }
+
